d6e6cb0611e09c6623fc86dc385c7fc669e767e2
Benjamin Newman <nlb9991@gmail.com>
2016-05-28 14:10:20 -0400

    Added addStore.html and results.html rendering

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
 from app import app
 from flask import render_template
 
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/addStore', methods=['GET','POST'])
+def addStore():
+	#form = AddStoreForm()
+	#if form.validate_on_submit()
+	return render_template('addStore.html')
+	
+@app.route('/results')
+def results():
+	return render_template('results.html')
